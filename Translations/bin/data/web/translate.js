0fbe8d37f2ab51f604d61146c1641aa0a3d85b35
Thomas Nicollet <thomas.nicollet@epitech.eu>
2018-03-29 12:31:12 +0200

    Maked translate working in a cluster

--- Translations/bin/data/web/translate.js
+++ Translations/bin/data/web/translate.js
@@ -1,48 +1,57 @@
-var translate = require('node-google-translate-skidz');
-var W3CWebSocket = require('websocket').w3cwebsocket;
-var client = new W3CWebSocket('ws://localhost:9092/');
+const cluster = require("cluster");
+const translate = require('node-google-translate-skidz');
+const W3CWebSocket = require('websocket').w3cwebsocket;
 
-client.onerror = function() {
-    console.log('Connection Error');
-};
+if (cluster.isMaster) {
+    console.log(`Master ${process.pid} is running`);
 
-client.onopen = function() {
-    console.log('WebSocket Client Connected');
-};
+    for (let i = 0; i < 1; i++) {
+        cluster.fork();
+    }
 
-client.onclose = function() {
-    console.log('Client Closed');
-};
+    cluster.on('exit', (deadWorker, code, signal) => {
+        var worker = cluster.fork();
+    });
 
-client.onmessage = function(e) {
-    
-    console.log("msg received" + e.data);
-    
-    // parse data
-    if (typeof e.data === 'string') {
-        
-        var splitted    = e.data.split("|");
-        var id          = splitted[0];
-        var type        = splitted[1];
-        var msg         = splitted[2];
-        var input       = splitted[3];
-        var output      = splitted[4];
-        
-        if(type == "RAW") {
-            
-            // go translate
-            translate({
-                text: msg,
-                source: input,
-                target: output
-            }, function(result) {
-                
-                client.send(id+"|TRANS|"+result["sentences"][0]["trans"]);
-                console.log(result["sentences"][0]["trans"]);
-                
-            });
-        
+} else {
+    var client = new W3CWebSocket('ws://localhost:9092/');
+
+    client.onerror = function() {
+        console.log('Connection Error');
+    };
+
+    client.onopen = function() {
+        console.log('WebSocket Client Connected');
+    };
+
+    client.onclose = function() {
+        console.log('Client Closed');
+        process.exit(1);
+    };
+
+    client.onmessage = function(e) {
+        console.log("msg received" + e.data);
+
+        if (typeof e.data === 'string') {
+
+            var splitted    = e.data.split("|");
+            var id          = splitted[0];
+            var type        = splitted[1];
+            var msg         = splitted[2];
+            var input       = splitted[3];
+            var output      = splitted[4];
+
+            if(type == "RAW") {
+
+                translate({
+                    text: msg,
+                    source: input,
+                    target: output
+                }, function(result) {
+                    client.send(id+"|TRANS|"+result["sentences"][0]["trans"]);
+                    console.log(result["sentences"][0]["trans"]);
+                });
+            }
         }
-        
     }
 };
