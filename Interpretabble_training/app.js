4d3a23d5e15f620cbcc037558efa4eeff865c0cc
erasme <erasme@Mac-mini-de-erasme.local>
2018-07-27 15:34:36 +0200

    add prediction

--- Interpretabble_training/app.js
+++ Interpretabble_training/app.js
@@ -2,14 +2,16 @@
 const http = require('http');
 const fs =require('fs');
 const staticServ = require('node-static');
+const spawn = require("child_process").spawn;
+
 
 var server = http.createServer(function (request, response) {
-  request.addListener('end', function () {
+	request.addListener('end', function () {
         fileServer.serve(request, response);
     }).resume();
 }),
-  io=require('socket.io').listen(server),
-  fileServer = new staticServ.Server('./');
+	io=require('socket.io').listen(server),
+	fileServer = new staticServ.Server('./');
 
 server.listen(8080);
 
@@ -20,28 +22,62 @@ function statPath(path) {
   return false;
 }
 
-var i=100;
+var i=0;
 io.sockets.on('connection', function (socket) {
-  socket.on('sImage', function(data){
-    var buf = new Buffer(data.image, 'base64'),
-      date = new Date(),
-      name = date.getFullYear()+date.getMonth().toString().replace(/^(\d)$/,'0$1')+date.getDate().toString().replace(/^(\d)$/,'0$1'),
-      path = "./public/Data/"+data.category+"/"+name+i.toString().replace(/^(\d)$/,'0$1')+".jpg",
-      exist = statPath(path);
+	socket.on('selectCat', function(data){
+	 	var buf = new Buffer(data.image, 'base64');
 
-    //verify file's existence
-    while(exist && exist.isFile()) {
-      i = i+1;
-      path = "./public/"+data.category+"/"+name+i.toString().replace(/^(\d)$/,'0$1')+".jpg";
-      exist = statPath(path);
-    }
-    try{
-      fs.writeFile(path, buf);
-      i = i+1;
-      socket.emit('confMsg',{'message' :"Le dessin a bien été enregistré en tant que " + data.category});
-    }catch(err){
-      socket.emit('confMsg',{'message' :"Impossible d'enregistrer le dessin"});
-      console.log(err);
-    }
-  })
+	    fs.writeFile("./public/output.jpg", buf,function(err){
+	    	if(err) throw(err);
+	    });
+
+	    var pythonProcess = spawn("python2", ["../Translations/prediction/scripts/label_image.py", "/Users/erasme/Desktop/interpretabble/Interpretabble_training/public/output.jpg"]);
+    
+		pythonProcess.stdout.on('data', (data) =>{
+			var rep = data.toString();
+			rep = rep.replace(/[\n]/gi, "" )
+			socket.emit('categoryPrediction',{'category' : rep});
+		});
+
+		/*pythonProcess.stderr.on('data', function(data){
+			console.log("err " + data);
+		});*/
+	});
+
+	socket.on('sImage', function(data){
+	  	var buf = new Buffer(data.image, 'base64');
+
+		var date = new Date(),
+	    	name = date.getFullYear()+date.getMonth().toString().replace(/^(\d)$/,'0$1')+date.getDate().toString().replace(/^(\d)$/,'0$1'),
+	    	path = "./public/Data/"+data.category,
+	    	//path = "../Translations/prediction/tf_files/drawings/"+data.category,
+	    	exist = statPath(path);
+
+	    //create category's folder
+	    if (exist == false){
+	    	fs.mkdirSync(path);
+	    	path = path +"/"+name+i.toString().replace(/^(\d)$/,'0$1')+".jpg";
+	    }else{
+	    	path = path +"/"+name+i.toString().replace(/^(\d)$/,'0$1')+".jpg";
+	    	exist = statPath(path);
+		    //verify file's existence
+    		while(exist && exist.isFile()) {
+		      i = i+1;
+		      path = "./public/Data/"+data.category+"/"+name+i.toString().replace(/^(\d)$/,'0$1')+".jpg";
+		      //path = "../Translations/prediction/tf_files/drawings/"+data.category+"/"+name+i.toString().replace(/^(\d)$/,'0$1')+".jpg",
+		      exist = statPath(path);
+		    }
+	    }
+	    
+	    fs.writeFile(path, buf, function(err){
+	    	if(err){
+	    		socket.emit('confMsg',{'message' :"Impossible d'enregistrer le dessin en tant que " + data.category});
+	    		console.log(err);
+	    		throw(err);
+	    	}else{
+	    		i = i+1;
+	    		socket.emit('confMsg',{'message' :"Le dessin a bien été enregistré en tant que " + data.category});
+	    	}
+	    });   
+  	})
 });
--- Interpretabble_training/public/assets/js/script.js
+++ Interpretabble_training/public/assets/js/script.js
@@ -13,16 +13,14 @@ $(function(){
 	var cursor = {};
 	var prev = {};
 
-
-
 	document.getElementById("paper").addEventListener('touchstart',function(e){
 		e.preventDefault();
-	//	alert(e.changedTouches[0].pageX+"  "+ canvas.offset().left );
+		//prev.x = e.pageX - canvas.offset().left ;
+		//prev.y = e.pageY - canvas.offset().top;
 		prev.x = e.changedTouches[0].pageX - canvas.offset().left ;
 		prev.y = e.changedTouches[0].pageY - canvas.offset().top;
 
 		drawing=true;
-
 		instructions.fadeOut();
 	});
 
@@ -31,13 +29,12 @@ $(function(){
 	});
 
 	document.getElementById("paper").addEventListener('touchmove',function(e){
-
-
 		//readjustement of the x and y (because of the margings)
 		var realX = e.changedTouches[0].pageX-canvas.offset().left,
 			realY = e.changedTouches[0].pageY-canvas.offset().top;
+		//var realX = e.pageX-canvas.offset().left,
+		//	realY = e.pageY-canvas.offset().top;
 		if(drawing){
-
 			drawLine(prev.x, prev.y, realX, realY);
 			prev.x = realX;
 			prev.y = realY;
@@ -45,67 +42,114 @@ $(function(){
 	});
 
 	function drawLine(fromx, fromy, tox, toy){
-    context.beginPath();
+		context.beginPath();
 		context.moveTo(fromx, fromy);
 		context.lineTo(tox, toy);
-    context.lineWidth= 5;
-    context.lineCap='round';
+		context.lineWidth= 5;
+		context.lineCap='round';
 		context.stroke();
 	}
+	//Hide the buttons of the categories
+	$(".col-md-3").hide();
 });
 
-var category;
+var category = null;
 
 function saveImage (data){
-  var url = 'http://192.168.81.35:8080',
-      socket = io.connect(url);
-  if (category==null){
-    alert("Vous devez indiquer la catégorie du dessin");
-  }else{
-    var canvas=$('#paper'),
-      context = canvas[0].getContext('2d');
-    if(confirm("Enregistrer le dessin en tant que "+ category+" ?")){
-			//set and fill background to white
-			context.globalCompositeOperation = "destination-over";
-			context.fillStyle = 'white';
-			context.fillRect(0,0,canvas[0].width,canvas[0].height);
+	
+	var url = location.host,
+  		socket = io.connect(url),
+  		canvas=$('#paper'),
+  		context = canvas[0].getContext('2d');
 
-      //send image Url to server
-      var dl = canvas[0].toDataURL('image/jpeg');
-			data = dl.replace(/^data:image\/\w+;base64,/, "");
-      socket.emit('sImage', {'image':data, 'category':category});
-		  socket.on('confMsg', function(msg){
+    //set and fill background to white
+	context.globalCompositeOperation = "destination-over";
+	context.fillStyle = 'white';
+	context.fillRect(0,0,canvas[0].width,canvas[0].height);
+
+  	//prepare image to send its Url to the server
+  	var dl = canvas[0].toDataURL('image/jpeg');
+	data = dl.replace(/^data:image\/\w+;base64,/, "");
+
+	//Define images's category through the prediction
+	if (category == null || category ==""){
+		socket.emit('selectCat', {'image':data, 'category':category});
+		socket.on('categoryPrediction', function(cat){
+			
+			if(confirm("Enregistrer le dessin en tant que "+ cat.category+" ?")){
+				category = cat.category;
+				socket.emit('sImage', {'image':data, 'category':category});
+				socket.on('confMsg', function(msg){
+					alert(msg.message);
+				});
+
+				//clear canvas and buttons
+				context.clearRect(0, 0, canvas[0].width, canvas[0].height);
+				//document.getElementById(category).disabled = false;
+				$('#instructions').fadeIn();
+				category = null;
+				$(".col-md-3").hide();
+
+			}else{
+				//Show buttons
+				$(".col-md-3").show();
+			}
+		});
+	//Define images's category through the buttons
+	}else{
+		if(confirm("Enregistrer le dessin en tant que "+ category+" ?")){
+			socket.emit('sImage', {'image':data, 'category':category});
+			socket.on('confMsg', function(msg){
 				alert(msg.message);
 			});
 
-      //clear canvas and buttons
-      context.clearRect(0, 0, canvas[0].width, canvas[0].height);
-      document.getElementById(category).disabled = false;
+			//clear canvas and buttons
+			context.clearRect(0, 0, canvas[0].width, canvas[0].height);
+			//document.getElementById(category).disabled = false;
+			document.getElementById("Autre").disabled = false;
 			$('#instructions').fadeIn();
-      category=null;
-    }
-  }
+			category = null;
+			$(".col-md-3").hide();
+
+		}else{
+			category = null;
+		}
+	}
 }
 
 function setCategory(obj){
-   category = document.getElementById(obj.id).value;
-   document.getElementById("Ambulance").disabled = false;
-   document.getElementById("Maison").disabled = false;
-   document.getElementById("Fleche").disabled = false;
-   document.getElementById("Biberon").disabled = false;
-   document.getElementById("Lit").disabled = false;
-   document.getElementById("Bouche").disabled = false;
-   document.getElementById("Lune").disabled = false;
-	 document.getElementById("Rien").disabled = false;
-   document.getElementById(obj.id).disabled = true;
+   	category = document.getElementById(obj.id).value;
+   	document.getElementById(obj.id).disabled = true;
+   	if (category == "Autre"){
+		category = prompt("Quelle catégorie est-ce ? ");
+	}
+	document.getElementById("Ambulance").disabled = false;
+	document.getElementById("Maison").disabled = false;
+	document.getElementById("Fleche").disabled = false;
+	document.getElementById("Biberon").disabled = false;
+	document.getElementById("Lit").disabled = false;
+	document.getElementById("Bouche").disabled = false;
+	document.getElementById("Lune").disabled = false;
+	document.getElementById("Nez").disabled = false;
+	document.getElementById("Yeux").disabled = false;
+	document.getElementById("Rien").disabled = false;
+	document.getElementById("Autre").disabled = false;
+	if (category !=null && category !=""){
+		category = category.substring(0,1).toUpperCase()+category.substring(1).toLowerCase();
+		saveImage();
+	}else{
+		document.getElementById(obj.id).disabled = false;
+		alert("Impossible d'enregistrer le dessin sans catégorie");
+	}
+		
 }
 
 document.getElementById('clear').addEventListener('click', function() {
-  var canvas=$('#paper'),
-		context = canvas[0].getContext('2d');
-  context.clearRect(0, 0, canvas[0].width, canvas[0].height);
+	var canvas=$('#paper'),
+  		context = canvas[0].getContext('2d');
+  	context.clearRect(0, 0, canvas[0].width, canvas[0].height);
 	$('#instructions').fadeIn();
 	if(category!=null) document.getElementById(category).disabled = false;
-	category=null;
-
+	else category = null;
+	$(".col-md-3").hide();
 }, false);
